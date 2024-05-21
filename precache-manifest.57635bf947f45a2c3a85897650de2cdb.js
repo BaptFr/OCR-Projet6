7fa1fe97116e9a14d5af8ae3704516c684a4381b
BaptFr <baptistesalazar@gmail.com>
2024-05-21 14:57:28 +0200

    Updates

---
+++ precache-manifest.57635bf947f45a2c3a85897650de2cdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee55a4bd9655968ec9e4c2b3d397d66",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "d2daefec96d2ad48ba8b",
+    "url": "/react-github-pages/static/css/main.b3312107.chunk.css"
+  },
+  {
+    "revision": "a7dde20dad57bb01aedf",
+    "url": "/react-github-pages/static/js/2.4819fb91.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/react-github-pages/static/js/2.4819fb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2daefec96d2ad48ba8b",
+    "url": "/react-github-pages/static/js/main.15b95e73.chunk.js"
+  },
+  {
+    "revision": "37268d8428d6fd597775",
+    "url": "/react-github-pages/static/js/runtime-main.777c759a.js"
+  },
+  {
+    "revision": "40349702c69af9d1b661fdc71d2b6a94",
+    "url": "/react-github-pages/static/media/about-banner-pic.40349702.jpg"
+  },
+  {
+    "revision": "6d416d00581d30d8bf9ab591b8c0a497",
+    "url": "/react-github-pages/static/media/home-banner-pic.6d416d00.jpg"
+  }
+]);
